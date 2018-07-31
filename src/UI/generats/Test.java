be1fc690b99ceac8dfdda735278bc27edd5508bd
annaFst <41938880+annaFst@users.noreply.github.com>
2018-07-31 21:51:52 +0300

    huhu

--- src/UI/generats/Test.java
+++ src/UI/generats/Test.java
@@ -3,6 +3,7 @@ package UI.generats;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Unmarshaller;
+import java.io.File;
 import java.io.InputStream;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -13,20 +14,29 @@ public class Test {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        String s ="D:/JavaProj/Ex1/ex1-error.xml";
-                //scan.nextLine();
+        String s = "D:/JavaProj/Ex1/ex1-error.xml";
+        //scan.nextLine();
         System.out.println(s);
-        InputStream input = Test.class.getResourceAsStream(s);
+        // InputStream input = Test.class.getClass().getResourceAsStream(s);
         try {
-            GameDescriptor desc = deserializeFrom(input);
+            GameDescriptor desc = parseXmltoJaxbMachine("D:/JavaProj/Ex1/ex1-error.xml");
             System.out.println(desc);
+            System.out.print(desc.getGame().getBoard().getColumns());
         } catch (JAXBException e) {
             e.printStackTrace();
         }
     }
-    private static GameDescriptor deserializeFrom(InputStream in) throws JAXBException {
-        JAXBContext jc = JAXBContext.newInstance("UI.generats");
-        Unmarshaller u = jc.createUnmarshaller();
-        return (GameDescriptor) u.unmarshal(in);
+
+    public static GameDescriptor parseXmltoJaxbMachine(String filePath) throws JAXBException {
+        Unmarshaller jaxbUnmarshaller = null;
+        GameDescriptor jaxbEnigma = null;
+        File xmlFilePath = new File("D:/JavaProj/Ex1/ex1-error.xml");
+        JAXBContext jaxbContext = null;
+        jaxbContext = JAXBContext.newInstance(GameDescriptor.class);
+        jaxbUnmarshaller = jaxbContext.createUnmarshaller();
+        jaxbEnigma = (GameDescriptor) jaxbUnmarshaller.unmarshal(xmlFilePath);
+        return jaxbEnigma;
+
     }
 }
+
