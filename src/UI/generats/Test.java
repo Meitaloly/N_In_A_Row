6a5a54cbf9e799ee08f306f5cfc133f595f2dfc3
annaFst <41938880+annaFst@users.noreply.github.com>
2018-07-31 17:50:02 +0300

    huhu

--- src/UI/generats/Test.java
+++ src/UI/generats/Test.java
@@ -1,32 +1,31 @@
-package UI;
+package UI.generats;
 
-import java.io.File;
-import java.io.InputStream;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.Scanner;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Unmarshaller;
 import java.io.InputStream;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Scanner;
 
 public class Test {
 
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        String s = scan.nextLine();
-        Path p  = Paths.get(s);
+        String s ="D:/JavaProj/Ex1/ex1-error.xml";
+                //scan.nextLine();
         System.out.println(s);
         InputStream input = Test.class.getResourceAsStream(s);
         try {
-            GameDescriptor desc =  deserializeFrom(input);
+            GameDescriptor desc = deserializeFrom(input);
+            System.out.println(desc);
         } catch (JAXBException e) {
             e.printStackTrace();
         }
     }
     private static GameDescriptor deserializeFrom(InputStream in) throws JAXBException {
-        JAXBContext jc = JAXBContext.newInstance("UI");
+        JAXBContext jc = JAXBContext.newInstance("UI.generats");
         Unmarshaller u = jc.createUnmarshaller();
         return (GameDescriptor) u.unmarshal(in);
     }
