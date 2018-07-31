07986a8555670a4a7a2ae4244c2c171f86646434
annaFst <41938880+annaFst@users.noreply.github.com>
2018-07-31 17:01:20 +0300

    huhu

--- src/UI/Test.java
+++ src/UI/Test.java
@@ -1,8 +1,33 @@
 package UI;
 
+import java.io.File;
+import java.io.InputStream;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Scanner;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+import java.io.InputStream;
+
 public class Test {
 
+
     public static void main(String[] args) {
-        System.out.println("kh");
+        Scanner scan = new Scanner(System.in);
+        String s = scan.nextLine();
+        Path p  = Paths.get(s);
+        System.out.println(s);
+        InputStream input = Test.class.getResourceAsStream(s);
+        try {
+            GameDescriptor desc =  deserializeFrom(input);
+        } catch (JAXBException e) {
+            e.printStackTrace();
+        }
+    }
+    private static GameDescriptor deserializeFrom(InputStream in) throws JAXBException {
+        JAXBContext jc = JAXBContext.newInstance("UI");
+        Unmarshaller u = jc.createUnmarshaller();
+        return (GameDescriptor) u.unmarshal(in);
     }
 }
