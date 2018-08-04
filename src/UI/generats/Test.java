1b31ec110ca275cd5d9432eaab441459aa6577cc
annaFst <41938880+annaFst@users.noreply.github.com>
2018-08-04 20:30:15 +0300

    huhu

--- src/UI/generats/Test.java
+++ src/UI/generats/Test.java
@@ -7,24 +7,21 @@ import java.io.File;
 import java.io.InputStream;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Test {
 
 
     public static void main(String[] args) {
-        Scanner s = new Scanner(System.in);
-        //String s = "D:/JavaProj/Ex1/ex1-error.xml";
-        String str = s.nextLine();
-        System.out.println(str);
-        // InputStream input = Test.class.getClass().getResourceAsStream(s);
-        int i = 0;
-        try {
-            GameDescriptor desc = parseXmltoJaxbMachine(str);
-            System.out.println(desc);
-            System.out.print(desc.getGame().getBoard().getColumns());
-        } catch (JAXBException e) {
-            e.printStackTrace();
+        char [][] array = new char [2][4];
+        Arrays.fill(array[0], '0');
+        Arrays.fill(array[1], '0');
+        for(int i = 0 ; i<2 ; i++){
+            for (int j=0; j<4;j++){
+                System.out.print (array[i][j]);
+            }
+            System.out.println ();
         }
     }
 
@@ -32,6 +29,7 @@ public class Test {
         Unmarshaller jaxbUnmarshaller = null;
         GameDescriptor jaxbEnigma = null;
         File xmlFilePath = new File(filePath);
+        System.out.println(xmlFilePath.exists());
         JAXBContext jaxbContext = null;
         jaxbContext = JAXBContext.newInstance(GameDescriptor.class);
         jaxbUnmarshaller = jaxbContext.createUnmarshaller();
